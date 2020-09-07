65c66dd953b214b79910b046088207cb291cc50a
henroyshaw <1326043418@qq.com>
2020-09-07 16:24:18 +0800

    5

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -2,7 +2,7 @@ package hellogit;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello git4");
+        System.out.println("Hello git5");
         
     }
 
